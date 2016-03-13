16acccf3bef765a1838fb36b227fcb1ea1256887
bill auger <mr.j.spam.me@gmail.com>
2016-03-12 21:27:02 -0500

    pruned API from lctv-badges project

---
+++ README.md
@@ -0,0 +1,43 @@
+LCTV Badges
+-----------
+
+License: GPLv3
+Requires: PHP v5.4+
+Version: 0.0.8
+
+#### SETUP
+
+Setup constants are in LctvApiCredentials.inc.
+This file must exist in order to use this class.
+You can create this file using LctvApiCredentials.inc.example as a reference
+and then initialize the API by browsing to authorize.php.
+See example.php for usage.
+
+
+* LCTV_CLIENT_ID
+This must match your app client id defined on the LCTV API website.
+
+* LCTV_CLIENT_SECRET
+This must match your app client secret defined on the LCTV API website.
+
+* LCTV_REDIRECT_URL is the url the API should return user authentication codes to.
+Should be the url to authorize.php and must match one that you defined
+on the LCTV API website.
+
+* LCTV_MASTER_USER is the master user account the API will use to make public
+queries. In order for any API request to work it needs an authorized user.
+You can authenticate this master account by visiting authorize.php.
+
+* LCTVAPI_STORAGE_CLASS is the class of the data store to use for caching.
+Options are LctvApiDataStoreFlatFiles or LctvApiDataStoreMySQL.
+
+* LCTV_DATA_PATH is the path to the folder where API tokens and cache will be
+stored if using the flat file data store. This folder must be writable by the server.
+It would be a good idea to use a folder below the server's public html path, so
+that cached user tokens aren't publicly accessible.
+
+* LCTVAPI_CACHE_EXPIRES_IN is the amount of time in seconds before cached
+data expires. Default is 300, or 5 minutes.
+
+* LCTVAPI_DB_NAME, LCTVAPI_DB_USER, LCTVAPI_DB_PASSWORD, LCTVAPI_DB_HOST are
+database connection options if using the MySQL data store.
--- authorize.php
+++ authorize.php
@@ -1,35 +1,120 @@
 <?php
 /**
  * Authorize Livecoding.tv accounts.
- * 
- * @package LCTVBadges\Authorize
+ *
+ * @package LctvApi\Authorize
  * @since 0.0.3
  */
 
+
 /** Initialize. */
-require_once( 'lctv_badges_init.php' );
+require( 'LctvApi.inc' );
 
-/** Load the API with no user. */
-$lctv_api = new LCTVAPI( array(
-	'data_store'    => LCTVAPI_DATA_STORE_CLASS,
-	'client_id'     => LCTV_CLIENT_ID,
-	'client_secret' => LCTV_CLIENT_SECRET,
-	'redirect_url'  => LCTV_REDIRECT_URL,
-	'user'          => '',
-) );
 
-/** Authorization html output. */
+if ( ( ! isset( $_GET['state'] ) || empty( $_GET['state'] ) ) &&
+     ( ! isset( $_GET['code'] ) || empty( $_GET['code'] ) ) ) {
+  if ( ! isset( $_GET['user'] ) || empty( $_GET['user'] ) ) {
 ?>
+    <script type="text/javascript">
+			var loc = window.location;
+			var this_location = loc.protocol + "//" + loc.host + loc.pathname;
+
+			do var user = prompt("Please enter your LCTV nick:", ""); while (user == null);
+			window.location = this_location + "?user=" + user;
+		</script>
+<?php
+		exit ;
+  }
+  else {
+  	$auth_user = htmlspecialchars( $_GET['user'] );
+  }
+}
+else {
+	$auth_user = '';
+}
+
+
+/** Load the API. */
+try {
+	$lctv_api = new LctvApi( $auth_user );
+}
+catch(Exception $ex) {
+	die($ex->getMessage());
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en-US">
 	<head>
 		<title>LCTV Badges Authorization</title>
 	</head>
 	<body style="font-family:Helvetica,Arial,sans-serif;font-size:15px;font-weight:400;color:#111;">
+
+
+<?php define(TRACE , false) ;
+if ( TRACE ) { ?>
+// begin DEBUG
+<pre>constants
+* <?php echo "LCTV_CLIENT_ID        = '" . LCTV_CLIENT_ID        . "'\n" ?>
+* <?php echo "LCTV_CLIENT_SECRET    = '" . LCTV_CLIENT_SECRET    . "'\n" ?>
+* <?php echo "LCTV_REDIRECT_URL     = '" . LCTV_REDIRECT_URL     . "'\n" ?>
+* <?php echo "LCTVAPI_STORAGE_CLASS = '" . LCTVAPI_STORAGE_CLASS . "'\n" ?>
+* <?php echo "LCTV_MASTER_USER      = '" . LCTV_MASTER_USER      . "'\n" ?>
+</pre>
+<pre>params
+<?php $is_state_set   = isset( $_GET['state'] ) ? "true" : "false";  ?>
+<?php $is_state_empty = empty( $_GET['state'] ) ? "true" : "false" ; ?>
+<?php $is_code_set    = isset( $_GET['code'] ) ? "true" : "false";   ?>
+<?php $is_code_empty  = empty( $_GET['code'] ) ? "true" : "false" ;  ?>
+<?php $is_user_set    = isset( $_GET['user'] ) ? "true" : "false";   ?>
+<?php $is_user_empty  = empty( $_GET['user'] ) ? "true" : "false" ;  ?>
+* <?php echo "isset(_GET['state']) = $is_state_set\n";               ?>
+* <?php echo "empty(_GET['state']) = $is_state_empty\n";             ?>
+* <?php echo "isset(_GET['code'])  = $is_code_set\n";                ?>
+* <?php echo "empty(_GET['code'])  = $is_code_empty\n";              ?>
+* <?php echo "isset(_GET['user'])  = $is_user_set\n";                ?>
+* <?php echo "empty(_GET['user'])  = $is_user_empty\n";              ?>
+* <?php echo "user=$auth_user\n";                                    ?>
+</pre>
+<?php $client_id     = (isset($lctv_api->client_id    )) ? $lctv_api->client_id     : "unset" ; ?>
+<?php $client_secret = (isset($lctv_api->client_secret)) ? $lctv_api->client_secret : "unset" ; ?>
+<?php $data_store    = (isset($lctv_api->data_store   )) ? $lctv_api->data_store    : "unset" ; ?>
+<?php $redirect_url  = (isset($lctv_api->redirect_url )) ? $lctv_api->redirect_url  : "unset" ; ?>
+<?php $scope         = (isset($lctv_api->scope        )) ? $lctv_api->scope         : "unset" ; ?>
+<?php $auth_user     = (isset($lctv_api->auth_user    )) ? $lctv_api->auth_user     : "unset" ; ?>
+<pre>library
+* <?php echo "lctv_api->client_id     = '$client_id'\n"                    ?>
+* <?php echo "lctv_api->client_secret = '$client_secret'\n"                ?>
+* <?php echo "lctv_api->redirect_url  = '$redirect_url'\n"                 ?>
+* <?php echo "lctv_api->data_store    = '" . LCTVAPI_STORAGE_CLASS . "'\n" ?>
+* <?php echo "lctv_api->scope         = '$scope'\n"                        ?>
+* <?php echo "lctv_api->auth_user     = '$auth_user'\n"                    ?>
+</pre>
+<?php
+$is_set= isset($lctv_api->token) ;
+$is_empty = empty($lctv_api->token) ;
+$token_dump = var_export($lctv_api->token , true) ;
+$token = ( ($is_set) ? (($is_empty) ? "empty" : $token_dump): "unset") ;
+$access_token = ((isset( $lctv_api->token) && isset( $lctv_api->token->access_token ) ) ? $lctv_api->token->access_token : "unset") ;
+?>
+<pre>tokens
+* <?php echo "token=$token"; ?>\n
+* <?php echo "access_token=$access_token"; ?>\n
+* <?php echo "LCTVAPI_STORAGE_PATH=" . LCTVAPI_STORAGE_PATH; ?>\n
+</pre>
+// end DEBUG
+<?php } ?>
+
+
 		<div style="padding:20px;width:600px;margin:0 auto;">
 		<?php if ( ! $lctv_api->is_authorized() ) : ?>
 			<h1 style="font-size:24px;font-weight:400;">Authorize</h1>
-			<p>Some badges require access to your Livecoding.tv account information. Authorize account access with the link below.</p>
+			<p>LCTV Badges use the Livecoding.tv API to get their relevant stats.
+					Badges that display data from public API endpoints require only the site admin to authorize this site.
+					Badges that display data from private API endpoints require each target user to authorize this site.
+					Vistors to the site will not need to authorize.
+					Authorize account access with the link below.</p>
 			<p><a href="<?php echo $lctv_api->get_authorization_url(); ?>">Connect your account</a></p>
 			<br><br>
 			<h1 style="font-size:24px;font-weight:400;">Remove Authorization</h1>
@@ -41,11 +126,11 @@ $lctv_api = new LCTVAPI( array(
 			<?php endif; ?>
 		<?php else : ?>
 			<h1 style="font-size:24px;font-weight:400;">Authorize</h1>
-			<p>The account &ldquo;<?php echo htmlspecialchars( $lctv_api->user ); ?>&rdquo; is now connected.</p>
+			<p>The account &ldquo;<?php echo htmlspecialchars( $lctv_api->auth_user ); ?>&rdquo; is now connected.</p>
 			<br><br>
 			<h1 style="font-size:24px;font-weight:400;">Remove Authorization</h1>
 			<p>This site saves an authorization token to access your Livecoding.tv account. You may delete this token so as to stop this site from further accessing your account information with the link below.</p>
-			<p><a href="https://themetuxedo-andtrev.c9.io/lctv-api/authorize.php?user=<?php echo urlencode( $lctv_api->user ); ?>&delete=<?php echo urlencode( $lctv_api->token->delete_id ); ?>">Disconnect your account</a></p>
+			<p><a href="<?php echo $lctv_api->redirect_url; ?>?user=<?php echo urlencode( $lctv_api->auth_user ); ?>&delete=<?php echo urlencode( $lctv_api->token->delete_id ); ?>">Disconnect your account</a></p>
 		<?php endif; ?>
 			<br><br>
 			<p><a href="./">Return Home</a></p>
